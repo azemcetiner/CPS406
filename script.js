f0e28fd0746e42adffb713c6450e7c9514370a1f
azemcetiner <azem_cetiner@hotmail.com>
2017-02-26 14:34:53 -0500

    removed validator

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 document.getElementById('main').innerHTML="hello world";
-
-function validateForm()
-{
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "")
-    {
-        alert("Name must be filled out");
-        return false;
-    }
-}
