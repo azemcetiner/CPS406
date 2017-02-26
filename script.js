401cce6324a157b1c4a8be5178775a08b2ae35df
azemcetiner <azem_cetiner@hotmail.com>
2017-02-26 13:54:14 -0500

    form test

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 document.getElementById('main').innerHTML="hello world";
+
+function validateForm()
+{
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "")
+    {
+        alert("Name must be filled out");
+        return false;
+    }
+}
